f8c2418a42ec5e7ee27673ec4c95751ba71b22c2
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-05-09 22:48:23 +0200

    Add Admin Check in Admin Table

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_05_09_001721) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "Admins"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
