13d193c8a09848b27c260a4481c04d501631d01a
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-05-11 18:03:42 +0200

    Editing Product Table And Set Relation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_many :brands
+    has_many :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :category
+    belongs_to :brand
     has_many :cart_items
     has_one_attached :image
 
