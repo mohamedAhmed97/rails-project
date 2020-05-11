7093eb3b89eacd218d69a64e029a4ee825c44ba9
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-05-11 20:06:30 +0200

    Fix error in Schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_05_10_233214) do
 
-<<<<<<< HEAD
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
@@ -30,11 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-<<<<<<< HEAD
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -44,11 +36,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-<<<<<<< HEAD
   create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -59,11 +47,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-<<<<<<< HEAD
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -76,11 +60,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -92,11 +72,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "name"
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -104,11 +80,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["category_id"], name: "index_brands_on_category_id"
   end
 
-<<<<<<< HEAD
   create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.integer "quantity"
     t.integer "product_id"
     t.integer "cart_id"
@@ -118,32 +90,20 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< HEAD
   create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.decimal "subtotal", precision: 10
     t.decimal "total", precision: 10
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< HEAD
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-<<<<<<< HEAD
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "title"
     t.decimal "price", precision: 10
     t.text "description"
@@ -155,11 +115,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-<<<<<<< HEAD
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-=======
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci", force: :cascade do |t|
->>>>>>> Users
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -173,10 +129,4 @@ ActiveRecord::Schema.define(version: 2020_05_10_233214) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "brands", "categories"
-<<<<<<< HEAD
-=======
-  add_foreign_key "carts", "users"
-  add_foreign_key "products", "brands"
-  add_foreign_key "products", "categories"
->>>>>>> Users
 end
